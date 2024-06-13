98e18244293cbfb9f1761ac2e3e6fc0efd1d87d9
ercan.kirbiyik <ercan.kirbiyik@enuygun.com>
2024-06-13 22:30:34 +0300

    DataTestId element locator added

---
+++ src/main/java/com/example/locators/DataTestIdElementLocator.java
@@ -0,0 +1,35 @@
+package com.example.locators;
+
+import com.example.annotations.FindByDataTestId;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+
+import java.lang.reflect.Field;
+
+public class DataTestIdElementLocator implements ElementLocator {
+
+    private final SearchContext searchContext;
+    private final By by;
+
+    public DataTestIdElementLocator(SearchContext searchContext, Field field) {
+        this.searchContext = searchContext;
+        FindByDataTestId findByDataTestId = field.getAnnotation(FindByDataTestId.class);
+        if (findByDataTestId != null) {
+            by = By.cssSelector(String.format("[data-testid='%s']", findByDataTestId.datatestid()));
+        } else {
+            throw new IllegalArgumentException("Field must be annotated with @FindByDataTestId");
+        }
+    }
+
+    @Override
+    public WebElement findElement() {
+        return searchContext.findElement(by);
+    }
+
+    @Override
+    public java.util.List<WebElement> findElements() {
+        return searchContext.findElements(by);
+    }
+}
