e8324092d5c8f9bb84bae272bc4dc3b75b50ee2e
ercan.kirbiyik <ercan.kirbiyik@enuygun.com>
2024-06-13 22:30:16 +0300

    DataTestId locator factory added

---
+++ src/main/java/com/example/locators/DataTestIdElementLocatorFactory.java
@@ -0,0 +1,21 @@
+package com.example.locators;
+
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+
+import java.lang.reflect.Field;
+
+public class DataTestIdElementLocatorFactory implements ElementLocatorFactory {
+
+    private final SearchContext searchContext;
+
+    public DataTestIdElementLocatorFactory(SearchContext searchContext) {
+        this.searchContext = searchContext;
+    }
+
+    @Override
+    public ElementLocator createLocator(Field field) {
+        return new DataTestIdElementLocator(searchContext, field);
+    }
+}
