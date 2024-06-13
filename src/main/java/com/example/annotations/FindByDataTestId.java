fb4439d776b5035b1541c3707ee8392d3dc37be1
ercan.kirbiyik <ercan.kirbiyik@enuygun.com>
2024-06-13 22:30:00 +0300

    FindByDataTestId interface added

---
+++ src/main/java/com/example/annotations/FindByDataTestId.java
@@ -0,0 +1,9 @@
+package com.example.annotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FindByDataTestId {
+    String datatestid();
+}
